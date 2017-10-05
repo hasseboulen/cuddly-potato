276ef5c820f4ed697b3ee471291086671882844a
benjoman92 <benjamin.brankovic@outlook.com>
2017-10-05 18:40:45 +0200

    add changes in .skin css and skin.js

--- imports/ui/components/common/Skin.js
+++ imports/ui/components/common/Skin.js
@@ -3,7 +3,7 @@ import * as React from 'react';
 const Skin = ({weapon, name, condition, collection}) => {
 	return (<div className="skin">
 		<h2>{name}</h2>
-		<p>vapen: {weapon}</p>
+		<p>vapen: <img src="https://steamcdn-a.akamaihd.net/apps/730/icons/econ/default_generated/weapon_awp_cu_awp_asimov_light_large.32d9045f8a2bcd13ca18390cc9fd82026e7195ab.png" />{weapon}</p>
 		<p>kondition: {condition}</p>
 		<p>samling: {collection}</p>
 	</div>);
